c42e75a4b1efe7653880b63defc6355727fbe69b
tianbo_xu2 <tianbo_xu2@apple.com>
2025-05-29 16:13:26 +0800

    update test_sum_squares

--- tests/test_sum_squares.py
+++ tests/test_sum_squares.py
@@ -2,10 +2,7 @@ import pytest
 
 def sum_squares(arr):
     """Sum the squares of the numbers in an array."""
-    total = 0
-    for x in arr:
-        total += x * x
-    return total
+    return sum(map(lambda x: x**2, arr))
 
 # Your tests can also be benchmarks
 @pytest.mark.benchmark
